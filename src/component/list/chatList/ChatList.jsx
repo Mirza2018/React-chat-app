2e9711f9768f9901926eb2599c763f526a4bef55
mirza2018 <mirza.eee.4th@gmail.com>
2024-08-04 17:36:23 +0600

    chatList

--- src/component/list/chatList/ChatList.jsx
+++ src/component/list/chatList/ChatList.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 import "./chatList.css"
 
 const ChatList = () => {
+    const [addMode,setAddMode]=useState(false)
     return (
         <div className='chatList'> 
+
            <div className="search">
+
             <div className="searchBar">
                 <img src="./search.png" alt="" />
                 <input type="text" placeholder='Search' />
             </div>
-            <img src="./plus.png" alt="" />
+
+            <img src={addMode?"./minus.png":"./plus.png"} alt="" className='add'
+            onClick={()=>setAddMode(!addMode)}/>
            </div>
+
+           <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Mirza 2</span>
+                <p>hello</p>
+            </div>
+           </div>
+           <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Mirza 2</span>
+                <p>hello</p>
+            </div>
+           </div>
+           <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Mirza 2</span>
+                <p>hello</p>
+            </div>
+           </div>
+
         </div>
     );
 };
