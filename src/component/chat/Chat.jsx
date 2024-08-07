7b18f43438211d0dd1dcbbf92c6cfb27fbd231d6
mirza2018 <mirza.eee.4th@gmail.com>
2024-08-07 17:34:43 +0600

    chat component center part

--- src/component/chat/Chat.jsx
+++ src/component/chat/Chat.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./chat.css"
+import EmojiPicker from 'emoji-picker-react';
 
 const Chat = () => {
+    const [open,setOpen]=useState(false)
+    const [text,setText]=useState("")
+
+    const handleEmoji=e=>{
+// console.log(e.emoji);
+setText(text+e.emoji)
+setOpen(!open)
+
+    }
+    // console.log(text);
+    
+
+
     return (
         <div className='chat'>
            <div className="top">
@@ -22,10 +36,85 @@ const Chat = () => {
 
 
            <div className="center">
+            <div className="message own">
+                <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message">
+            <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message own">
+            <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message">
+            <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message own">
+            <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message">
+            <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
+            <div className="message own">
+            <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    <img src="https://cdn2.unrealengine.com/world-of-goo-2-1-2880x1620-7e18f4b1ae55.jpg" alt="" />
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <span>1 min ago</span>
+                </div>
+            </div> 
+
+            
+            <div className="message">
+            <img src="./avatar.png" alt="" />
+                <div className="texts">
+                    
+                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    <span>1 min ago</span>
+                </div>
+            </div>
 
            </div>
-           <div className="bottom">
 
+
+           <div className="bottom">
+            <div className="icons">
+                <img src="./img.png" alt="" />
+                <img src="./camera.png" alt="" />
+                <img src="./mic.png" alt="" />
+            </div>
+            <input type="text" placeholder='Type a message...' onChange={e=>setText(e.target.value)}  value={text}/>
+            <div className="emoji">
+                <img onClick={()=>setOpen(!open)} src="./emoji.png" alt="" />
+                <div className="picker">
+                     <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+                </div>
+               
+            </div>
+            <button className='sendButton'>send</button>
            </div>
         </div>
     );
