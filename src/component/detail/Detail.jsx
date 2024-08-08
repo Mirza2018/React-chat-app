bc5fa3e0d5df0a9f88ebe4e99f2d3ad1da3e1739
mirza2018 <mirza.eee.4th@gmail.com>
2024-08-08 11:48:48 +0600

    detail component

--- src/component/detail/Detail.jsx
+++ src/component/detail/Detail.jsx
@@ -3,7 +3,96 @@ import "./detail.css"
 const Detail = () => {
     return (
         <div className='detail'>
-            Detail
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <p>mirza 3</p>
+                <span>Lorem ipsum dolor sit amet consectetur.</span>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>
+                            Chat Settings
+                        </span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>
+                            Chat Settings
+                        </span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>
+                            Privacy & help
+                        </span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>
+                            Shared photos
+                        </span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                    <div className="photos">
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://cdn2.unrealengine.com/world-of-goo-2-1-2880x1620-7e18f4b1ae55.jpg" alt="" />
+                            <span>
+                                Photo_goo.png
+                            </span> 
+                            </div>
+                            <img src="./download.png"  className="icon" alt="" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://cdn2.unrealengine.com/world-of-goo-2-1-2880x1620-7e18f4b1ae55.jpg" alt="" />
+                            <span>
+                                Photo_goo.png
+                            </span> 
+                            </div>
+                            <img src="./download.png"  className="icon" alt="" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://cdn2.unrealengine.com/world-of-goo-2-1-2880x1620-7e18f4b1ae55.jpg" alt="" />
+                            <span>
+                                Photo_goo.png
+                            </span> 
+                            </div>
+                            <img src="./download.png"  className="icon" alt="" />
+                        </div>
+                        <div className="photoItem">
+                            <div className="photoDetail">
+                                <img src="https://cdn2.unrealengine.com/world-of-goo-2-1-2880x1620-7e18f4b1ae55.jpg" alt="" />
+                            <span>
+                                Photo_goo.png
+                            </span> 
+                            </div>
+                            <img src="./download.png"  className="icon" alt="" />
+                        </div>
+
+                       
+                    </div>
+                </div>
+
+
+                <div className="option">
+                    <div className="title">
+                        <span>
+                            Sherad Files 
+                        </span>
+                        <img src="./arrowDown.png" alt="" />
+                    </div>
+                </div>
+                <button>Block User</button>
+            </div>
         </div>
     );
 };
