e69460539335ab0fe916f7d2cf2114572a62a6a6
mirza2018 <mirza.eee.4th@gmail.com>
2024-08-16 18:41:53 +0600

    user search from firestor

--- src/component/list/chatList/ChatList.jsx
+++ src/component/list/chatList/ChatList.jsx
@@ -13,21 +13,25 @@ const ChatList = () => {
     const {currentUser}=useUserStore()
 
     useEffect(()=>{
-const unSub=onSnapshot(doc(db,"userChats",currentUser.id),async (res)=>{
-    const items=res.data().chats
-    
-const promises=items.map(async(item)=>{
-const userDocRef=doc(db,"users",item.receiverId);
-const userDocSnap=await getDoc(userDocRef)
 
-const user=userDocSnap.data();
-return {...item,user};
-})
 
-const chatData=await Promise.all(promises)
-setChats(chatData.sort((a,b)=>b.updatedAt-a.updatedAt))
+    const unSub = onSnapshot(doc(db,"userchats",currentUser.id),async (res)=>{
+        const items=res.data()?.chats
+        
+    const promises=items.map(async(item)=>{
+    const userDocRef=doc(db,"users",item.receiverId);
+    const userDocSnap=await getDoc(userDocRef)
 
-})
+    const user=userDocSnap.data();
+
+    return { ...item,user };
+
+    });
+
+    const chatData=await Promise.all(promises)
+    setChats(chatData.sort((a,b)=>b.updatedAt-a.updatedAt))
+
+    })
 
 return ()=>{
     unSub()
--- src/component/list/chatList/addUser/AddUser.jsx
+++ src/component/list/chatList/addUser/AddUser.jsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './addUser.css'
+import { db } from '../../../../library/firebase';
+import { collection, getDoc, getDocs, query, where } from 'firebase/firestore';
 
 const AddUser = () => {
+
+    const [user,setUser]=useState(null)
+
+
+    const handleSearch=async (e)=>{
+       e.preventDefault() 
+       const formData=new FormData(e.target)
+    //    const {userName}=Object.fromEntries(formData)
+    const username=formData.get("username")
+
+     try {
+        const userRef=collection(db,"users");
+
+        const q=query(userRef,where("username","==",username))
+
+        const querySnapShort=await getDocs(q)
+        if(!querySnapShort.empty){
+            setUser(querySnapShort.docs[0].data())
+        }
+
+     } catch (err) {
+        console.log(err);
+        
+     }
+       
+    }
+
     return (
         <div className='addUser'>
-            <form action="">
-                <input type="text" placeholder='UserName' name='userName' />
+            <form onSubmit={handleSearch}>
+                <input type="text" placeholder='UserName' name='username' />
                 <button className='first'>Search</button>
             </form>
-            <div className="user">
+            {user && <div className="user">
                 <div className="detail">
-                    <img src="./avatar.png" alt="" />
-                    <span>Mirza 007</span>
+                    <img src={user.avatar || "./avatar.png"} alt="" />
+                    <span>{user.username}</span>
                 </div>
                 <button>Add User</button>
-            </div>
+            </div>}
         </div>
     );
 };
